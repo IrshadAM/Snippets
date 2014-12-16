a2f7de30cec6d8df577c69a154592082010d6076
IrshadAM <irshad.mallick@hotmail.com>
2014-12-16 13:39:34 +0300

    made textToArray neater

--- textToArray.js
+++ textToArray.js
@@ -2,17 +2,16 @@ function textToArray(string, reverse) {
   var stringLength = string.length;
   var array = new Array;
 
-  if(!reverse || typeof reverse == undefined){
+  if (!reverse || typeof reverse == undefined) {
     for (var i = 0; i < stringLength; i++) {
       array.push(string.substring(i, i + 1));
     }
     return array;
-  }else if(reverse){
+  } else if (reverse) {
     for (var i = 0; i < stringLength; i++) {
       array.unshift(string.substring(i, i + 1));
     }
     return array;
   }
 }
-
-console.log(textToArray("Hello", false)); //Make second param true for reversed array
+console.log(textToArray('Hello', false)); //Make second param true for reversed array
